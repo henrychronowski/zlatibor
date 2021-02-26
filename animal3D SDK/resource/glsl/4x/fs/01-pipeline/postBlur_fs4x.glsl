e48a2696553046547d626d7e8e630358c868bde1
Henry Chronowski <henry.chronowski@mymail.champlain.edu>
2021-02-26 03:36:18 -0500

    Adding working blur

--- animal3D SDK/resource/glsl/4x/fs/01-pipeline/postBlur_fs4x.glsl
+++ animal3D SDK/resource/glsl/4x/fs/01-pipeline/postBlur_fs4x.glsl
@@ -17,6 +17,8 @@
 /*
 	animal3D SDK: Minimal 3D Animation Framework
 	By Daniel S. Buckstein
+
+	Based off of information in the blue book
 	
 	postBlur_fs4x.glsl
 	Gaussian blur.
@@ -31,21 +33,61 @@
 //		(hint: the efficiency of this is described in class)
 
 in vec2 vTexcoord;
+in vec4 vTexcoord_atlas;
 
 uniform vec2 uAxis;
 
 layout (location = 0) out vec4 rtFragColor;
 
+uniform sampler2D uTex_dm;
+uniform sampler2D uImage00;
 
+// From the blue book listing 9.27
+const float weights[] = float[](0.0024499299678342,
+0.0043538453346397,
+0.0073599963704157,
+0.0118349786570722,
+0.0181026699707781,
+0.0263392293891488,
+0.0364543006660986,
+0.0479932050577658,
+0.0601029809166942,
+0.0715974486241365,
+0.0811305381519717,
+0.0874493212267511,
+0.0896631113333857,
+0.0874493212267511,
+0.0811305381519717,
+0.0715974486241365,
+0.0601029809166942,
+0.0479932050577658,
+0.0364543006660986,
+0.0263392293891488,
+0.0181026699707781,
+0.0118349786570722,
+0.0073599963704157,
+0.0043538453346397,
+0.0024499299678342);
 
 void main()
 {
-	// DUMMY OUTPUT: all fragments are OPAQUE AQUA
-	rtFragColor = vec4(0.0, 1.0, 0.5, 1.0);
-
 	// blurring along an axis
 	// -> sample neigboring pixels, output weighted average
 	//		-> coordinate offset by some amount (add/sub displacement vector)
 	//			-> example: horizontal, dv = vec2(1 / resolution (width), 0)
 	//			-> example: vertical, dv = vec2(0, 1 / height)
+
+	int i;
+	vec3 col = vec3(0.0);
+	// Get displacement vector
+	vec2 offset = vTexcoord_atlas.xy - (uAxis * 14.0);
+
+	// Add offset samples to the fragment color
+	for(i = 0; i < weights.length(); i++)
+	{
+		//Tried to use texelFetch as the blue book does, but it was uncooperative so switched to just texture
+		col += texture(uTex_dm, offset + (uAxis * i)).rgb * weights[i];
+	}
+
+	rtFragColor = vec4(col, 1.0);
 }
