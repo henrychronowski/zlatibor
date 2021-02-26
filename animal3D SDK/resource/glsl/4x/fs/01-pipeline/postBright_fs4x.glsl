877360d805bff7aa35d452a79643aa09438ebb5a
Henry Chronowski <henry.chronowski@mymail.champlain.edu>
2021-02-26 02:33:11 -0500

    Adding semi-functional bright pass

--- animal3D SDK/resource/glsl/4x/fs/01-pipeline/postBright_fs4x.glsl
+++ animal3D SDK/resource/glsl/4x/fs/01-pipeline/postBright_fs4x.glsl
@@ -32,27 +32,31 @@
 
 layout (location = 0) out vec4 rtFragColor;
 
-//in vec4 vTexcoord;
+in vec2 vTexcoord;
+in vec4 vTexcoord_atlas;
 
 //uniform float uBrightThreshold;
 
 //uniform sampler2D uImage00;
+uniform sampler2D uTex_dm;
 
 void main()
 {
 	// DUMMY OUTPUT: all fragments are OPAQUE ORANGE
 	//rtFragColor = vec4(1.0, 0.5, 0.0, 1.0);
 
-	vec3 luminanceFormula = vec3(0.2126, 0.7152, 0.0722);	// https://en.wikipedia.org/wiki/Relative_luminance
+	// vec3 luminanceFormula = vec3(0.2126, 0.7152, 0.0722);	// https://en.wikipedia.org/wiki/Relative_luminance
+	vec3 luminanceFormula = vec3(0.299, 0.587, 0.144);	// From the blue book listing 9.26
 
 
-	//vec4 color = texture2D(uImage00, vTexcoord.xy);
-	//float luminance = dot(luminanceFormula, color.xyz);
+//	vec3 color = texture2D(uImage00, vTexcoord).rgb;
+	vec3 color = texture2D(uTex_dm, vTexcoord_atlas.xy).rgb;
+	float luminance = dot(luminanceFormula, color.xyz);
 	//luminance = max (0.0, luminance);	
-	//// luminance = max(0.0, luminance - uBrightThreshold); // Allows for a threshold value
-	//color.xyz *= sign(luminance);
+	// luminance = max(0.0, luminance - uBrightThreshold); // Allows for a threshold value
+	color *= (3.0 - (2 * luminance)) * luminance * luminance;
 
-	//gl_FragColor = color;
-	gl_FragColor = vec4(1.0, 0.5, 0.0, 1.0);
+	rtFragColor = vec4(color, 1.0f);
+	//gl_FragColor = vec4(1.0, 0.5, 0.0, 1.0);
 
 }
