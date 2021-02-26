341174a3dcdf820a50836d39edc89da94806fa8d
Henry Chronowski <henry.chronowski@mymail.champlain.edu>
2021-02-26 03:35:43 -0500

    Adding working bright

--- animal3D SDK/resource/glsl/4x/fs/01-pipeline/postBright_fs4x.glsl
+++ animal3D SDK/resource/glsl/4x/fs/01-pipeline/postBright_fs4x.glsl
@@ -42,21 +42,13 @@ uniform sampler2D uTex_dm;
 
 void main()
 {
-	// DUMMY OUTPUT: all fragments are OPAQUE ORANGE
-	//rtFragColor = vec4(1.0, 0.5, 0.0, 1.0);
-
 	// vec3 luminanceFormula = vec3(0.2126, 0.7152, 0.0722);	// https://en.wikipedia.org/wiki/Relative_luminance
 	vec3 luminanceFormula = vec3(0.299, 0.587, 0.144);	// From the blue book listing 9.26
 
 
-//	vec3 color = texture2D(uImage00, vTexcoord).rgb;
 	vec3 color = texture2D(uTex_dm, vTexcoord_atlas.xy).rgb;
 	float luminance = dot(luminanceFormula, color.xyz);
-	//luminance = max (0.0, luminance);	
-	// luminance = max(0.0, luminance - uBrightThreshold); // Allows for a threshold value
-	color *= (3.0 - (2 * luminance)) * luminance * luminance;
+	color *= 3.0 * luminance * luminance; // This was found too look cool from experimentation initially based on the formula in the blue book
 
 	rtFragColor = vec4(color, 1.0f);
-	//gl_FragColor = vec4(1.0, 0.5, 0.0, 1.0);
-
 }
