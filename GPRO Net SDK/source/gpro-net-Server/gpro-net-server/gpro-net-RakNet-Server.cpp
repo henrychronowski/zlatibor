9b12f3a0b921c2776f79cba807451f6a5f24ff5c
henry.chronowski <henry.chronowski@mymail.champlain.edu>
2021-04-25 13:39:21 -0400

    Adding first recieve uniform packet recieve

--- GPRO Net SDK/source/gpro-net-Server/gpro-net-server/gpro-net-RakNet-Server.cpp
+++ GPRO Net SDK/source/gpro-net-Server/gpro-net-server/gpro-net-RakNet-Server.cpp
@@ -75,12 +75,17 @@ namespace gproNet
 			ReadTest(bitstream);
 			WriteTest(bitstream_w, "Hello client from server");
 			peer->Send(&bitstream_w, MEDIUM_PRIORITY, UNRELIABLE_SEQUENCED, 0, sender, false);
+
+			WriteTimestamp(bitstream_w);
+			bitstream_w.Write((RakNet::MessageID)ID_GPRO_SEND_UNIFORM);
+			peer->Send(&bitstream_w, MEDIUM_PRIORITY, UNRELIABLE_SEQUENCED, 0, sender, false);
 		}	return true;
 
 		case ID_GPRO_RECIEVE_UNIFORM:
 		{
-			RakNet::BitStream uIn;
-
+			RakNet::RakString rs;
+			bitstream.Read(rs);
+			printf("%s\n", rs.C_String());
 		}
 
 		}
