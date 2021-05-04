45f5d3144404d4f5485a14562fd6763134117fe1
henry.chronowski <henry.chronowski@mymail.champlain.edu>
2021-05-03 22:06:55 -0400

    Adding initial custom shader GUI

---
+++ Graphics2Final/Assets/Scripts/CustomShaderGui.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+// A custom editor interface for shaders, drawing from the editor for default shaders
+// Written by Henry Chronowski
+// Referenced https://catlikecoding.com/unity/tutorials/rendering/part-9/
+
+public class CustomShaderGui : ShaderGUI
+{
+	static GUIContent staticLabel = new GUIContent();
+
+	MaterialEditor editor;
+	MaterialProperty[] properties;
+
+	MaterialProperty FindProperty(string name)
+	{
+		return FindProperty(name, properties);
+	}
+
+	static GUIContent MakeLabel( string text, string tooltip = null)
+	{
+		staticLabel.text = text;
+		staticLabel.tooltip = tooltip;
+		return staticLabel;
+	}
+
+	public override void OnGUI(MaterialEditor materialEditor, MaterialProperty[] properties)
+	{
+		this.editor = materialEditor;
+		this.properties = properties;
+
+		MainMaps();
+	}
+
+	void MainMaps()
+	{
+		GUILayout.Label("Main Maps", EditorStyles.boldLabel);
+
+		MaterialProperty mainTex = FindProperty("uTexture");
+		editor.TexturePropertySingleLine(MakeLabel("Albedo", "Albedo (RGB)"), mainTex, FindProperty("uColor"));
+
+		//MetallicMap();
+		//Smoothness();
+		NormalMap();
+
+		editor.TextureScaleOffsetProperty(mainTex);
+	}
+
+	void NormalMap()
+	{
+		MaterialProperty map = FindProperty("uNormalMap");
+		editor.TexturePropertySingleLine(MakeLabel("Normal Map"), map, map.textureValue ? FindProperty("uBumpScale") : null);
+	}
+
+	void MetallicMap()
+	{
+		MaterialProperty slider = FindProperty("_Metallic");
+		EditorGUI.indentLevel += 2;
+		editor.ShaderProperty(slider, MakeLabel("Metallic"));
+		EditorGUI.indentLevel -= 2;
+	}
+
+	void Smoothness()
+	{
+		MaterialProperty slider = FindProperty("_Smoothness");
+		EditorGUI.indentLevel += 2;
+		editor.ShaderProperty(slider, MakeLabel("slider"));
+		EditorGUI.indentLevel -= 2;
+	}
+}
